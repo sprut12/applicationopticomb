6748e6b55e4de745e6f5d27e07436467d76fe3fc
sprut12 <sprot35@gmail.com>
2016-10-24 21:05:13 +0300

    Add files via upload

--- www/js/index.js
+++ www/js/index.js
@@ -70,21 +70,21 @@ var app = {
             "Format: " + result.format + "\n" + 
             "Cancelled: " + result.cancelled);  
 
-           console.log("Scanner result: \n" +
+            /* console.log("Scanner result: \n" +
                 "text: " + result.text + "\n" +
                 "format: " + result.format + "\n" +
                 "cancelled: " + result.cancelled + "\n");
             document.getElementById("info").innerHTML = result.text;
             console.log(result);
 			window.open('http://www.opti-com.ru/catalog/online/categories/product-%D0%900000002832/', '_blank', 'location=yes');
-            /*
+            
             if (args.format == "QR_CODE") {
                 window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
             }
             */
 
         }, function (error) { 
-            console.log("Scanning failed: ", error); 
+            alert("Scanning failed: ", error); 
         } );
     },
 	
@@ -105,7 +105,7 @@ var app = {
 					);
 			}, 
 			function (error) { 
-           			console.log("Scanning failed: ", error); 
+           			alert("Scanning failed: ", error); 
         		} 
 		);	
 		},
